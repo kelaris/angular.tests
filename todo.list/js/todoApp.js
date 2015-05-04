0a553b10b13dcf6fef0036d3b84c7eb0f040df8b
kelariss@gmail.com <kelariss@gmail.com>
2015-05-04 12:54:37 +0700

    Added todo and tommatoe Api

---
+++ todo.list/js/todoApp.js
@@ -0,0 +1,19 @@
+﻿var todoApp = angular.module('ToDoList', []);
+
+todoApp.controller("toDoController", ['$scope', function($scope) {
+
+    $scope.toDoItems = [
+        { title: "First simple to do item", done: false }
+    ];
+        
+    $scope.addToDoItem = function () {
+        $scope.toDoItems.unshift({ title: $scope.new_todo, done: false });
+        $scope.new_todo = '';
+    };
+
+    $scope.removeToDoItem = function(item) {
+        var index = $scope.toDoItems.indexOf(item);
+        $scope.toDoItems.splice(index, 1);
+    };
+
+}]);
---
+++ tomatoe.api/js/xhrTestApp.js
@@ -0,0 +1,81 @@
+﻿var rottenTomatoesAPIKey = "6n63ccmvb2vrkewaay4pf9q2";
+var delayInMs = 200;
+var timeoutPromise, request;
+
+var xhrTestApp = angular.module('xhrTest', []);
+
+xhrTestApp.controller("searchController", function ($scope, $timeout, rottenTomatoesDataFactory) {
+
+    $scope.error_text = "";
+
+    $scope.search_results = [];
+
+    $scope.clearSearchText = function () {
+        $scope.search_text = '';
+    };
+    
+    $scope.$watch('search_text', function (newValue) {
+
+        $scope.error_text = "";
+        $timeout.cancel(timeoutPromise);
+        if (request) {
+            request.cancel("User cancelled");
+        }
+
+        if (typeof newValue != "undefined" && newValue && newValue.length > 2) {
+
+            timeoutPromise = $timeout(function() {
+                //Using timeout for prevent many requests to the server while user is typing
+                request = rottenTomatoesDataFactory.search(newValue);
+
+                request.promise.then(function (result) {
+                    $scope.search_results = result.movies;
+                }, function (reason) {
+                    if (status != 0) { //if not canceled  
+                        $scope.error_text = "Http Error code: " + reason.status;
+                    }
+                    console.log(reason);
+                });
+
+            }, delayInMs);
+
+        } else {
+            $scope.search_results = [];
+        }
+    });
+});
+
+xhrTestApp.factory("rottenTomatoesDataFactory", function ($http, $q) {
+
+    var search = function (query) {
+        var canceller = $q.defer();
+
+        var cancel = function (reason) {
+            canceller.resolve(reason);
+        };
+
+        var promise =
+            $http.jsonp('http://209.237.233.58/api/public/v1.0/movies.json', {
+                params: {
+                    page_limit: 6,
+                    page: 1,
+                    q: query,
+                    apikey: rottenTomatoesAPIKey,
+                    callback: 'JSON_CALLBACK'
+                },
+                timeout: canceller.promise
+            }).then(function (response) {
+                return response.data;
+            });
+
+        return {
+            promise: promise,
+            cancel: cancel
+        };
+    };
+
+    return {
+        search: search
+    };
+
+});
